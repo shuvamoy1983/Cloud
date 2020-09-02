ee94a971d2a310711033e9ebf2556a9813d2ca03
shuvamoy008 <64772923+shuvamoy008@users.noreply.github.com>
2020-09-02 21:39:32 +0530

    Update aws_bash.sh

--- aws_bash.sh
+++ aws_bash.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-./config.sh
-#cd $1
-#terraform init -input=false
-#terraform plan -out=tfplan -input=false -lock=false
-#terraform apply tfplan
+
+cd $1
+terraform init -input=false
+terraform plan -out=tfplan -input=false -lock=false
+terraform apply tfplan
