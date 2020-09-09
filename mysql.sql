f240e00cbf3d3b821c9101793831115c6daca074
tech <shuvamoy1983@gmail.com>
2020-09-09 08:01:17 -0700

    Added Cassandra

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,4 @@
+## Add your table 
 create database mydb;
 CREATE TABLE mydb.emp ( id int, Name varchar(20), Salary int );
 CREATE TABLE mydb.dept ( DEPT_ID int, Dept_Name varchar(20), EMP_ID int );
